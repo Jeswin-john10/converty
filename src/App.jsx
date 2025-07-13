2aed17863e61886a31b06e903d3b860a68e79bb4
jeswin john <jeswinjohn03@gmail.com>
2025-07-13 12:32:21 +0530

    header update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-
-
 import './App.css'
 import Header from './component/Header'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Footer from './component/Footer'
+import usePageTracking from './hooks/usePageTracking'
+
+// Pages
 import Home from './pages/Home'
 import PricingPage from './pages/PricingPage'
 import DeveloperSupport from './pages/DeveloperSupport'
-import Footer from './component/Footer'
 import SponsorPage from './pages/SponsorPage'
 import TermsPage from './pages/TermsPage'
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage'
@@ -15,6 +16,8 @@ import ContactPage from './pages/ContactPage'
 import ToolListPage from './pages/ToolListPage'
 import ConversionFeaturesPage from './pages/ConversionFeaturesPage'
 import FaqPage from './pages/FaqPage'
+
+// Converters
 import ExcelToJsonConverter from './pages/ExcelToJsonConverter'
 import JsonToExcelConverter from './pages/JsonToExcelConverter'
 import CsvToJsonConverter from './pages/CsvToJsonConverter'
@@ -29,45 +32,43 @@ import BarcodeTool from './pages/BarcodeTool'
 import Base64ToText from './pages/Base64ToText'
 import TextEncoderTool from './pages/TextEncoderTool'
 
-
 function App() {
+  usePageTracking(); // ⬅ Track GA page views
 
   return (
     <>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/pricing' element={<PricingPage/>}/>
-            <Route path='/developer-support' element={<DeveloperSupport/>}/>
-                  <Route path='/sponsor' element={<SponsorPage/>}/>
-<Route path='/terms' element={<TermsPage/>}/>
-<Route path='/privacy' element={<PrivacyPolicyPage/>}/>
-<Route path='/about' element={<AboutPage/>}/>
-<Route path='/contact' element={<ContactPage/>}/>
-<Route path='/all-convertion-tool' element={<ToolListPage/>}/>
-<Route path='/all-convertion-features' element={<ConversionFeaturesPage/>}/>
-<Route path='/Faq' element={<FaqPage/>}/>
-{/* pages */}
-<Route path='/excel-to-json' element={<ExcelToJsonConverter/>}/>
-<Route path='/json-to-excel' element={<JsonToExcelConverter/>}/>
-<Route path='/csv-to-json' element={<CsvToJsonConverter/>}/>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pricing' element={<PricingPage />} />
+        <Route path='/developer-support' element={<DeveloperSupport />} />
+        <Route path='/sponsor' element={<SponsorPage />} />
+        <Route path='/terms' element={<TermsPage />} />
+        <Route path='/privacy' element={<PrivacyPolicyPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/all-convertion-tool' element={<ToolListPage />} />
+        <Route path='/all-convertion-features' element={<ConversionFeaturesPage />} />
+        <Route path='/Faq' element={<FaqPage />} />
 
-<Route path='/csv-to-markdown' element={<CsvToMarkdownConverter/>}/>
-<Route path='/csv-to-sql' element={<CsvToSqlConverter/>}/>
-
-<Route path='/csv-to-html' element={<CsvToHtmlTableConverter/>}/>
-<Route path='/json-to-yaml' element={<JsonToYamlConverter/>}/>
-<Route path='/html-to-markdown' element={<HtmlToMarkdownConverter/>}/>
-<Route path='/qr-generator' element={<QrCodeGenerator/>}/>
-<Route path='/qr-scanner' element={<QrCodeScanner/>}/>
-<Route path='/barcode-to-text' element={<BarcodeTool/>}/>
-<Route path='/base64-decode' element={<Base64ToText/>}/>
-<Route path='/text-encoder' element={<TextEncoderTool/>}/>
-
-    </Routes>
-    <Footer/>
+        {/* Tools */}
+        <Route path='/excel-to-json' element={<ExcelToJsonConverter />} />
+        <Route path='/json-to-excel' element={<JsonToExcelConverter />} />
+        <Route path='/csv-to-json' element={<CsvToJsonConverter />} />
+        <Route path='/csv-to-markdown' element={<CsvToMarkdownConverter />} />
+        <Route path='/csv-to-sql' element={<CsvToSqlConverter />} />
+        <Route path='/csv-to-html' element={<CsvToHtmlTableConverter />} />
+        <Route path='/json-to-yaml' element={<JsonToYamlConverter />} />
+        <Route path='/html-to-markdown' element={<HtmlToMarkdownConverter />} />
+        <Route path='/qr-generator' element={<QrCodeGenerator />} />
+        <Route path='/qr-scanner' element={<QrCodeScanner />} />
+        <Route path='/barcode-to-text' element={<BarcodeTool />} />
+        <Route path='/base64-decode' element={<Base64ToText />} />
+        <Route path='/text-encoder' element={<TextEncoderTool />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App 
+export default App;
